d1447ddbce0db24f8d20a612b616cc0b7f34b7d8
thalyson004 <thalyson.uece@gmail.com>
2019-09-20 21:28:29 -0300

    Create H.cpp

---
+++ SBC/SBC2019/H.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define F first
+#define S second
+#define ii pair<int,int>
+#define iii pair<ii, int>
+#define iiii pair<ii, ii>
+#define viiii vector< iiii >
+#define viii vector< iii >
+#define vii vector< ii >
+#define vi vector< int >
+#define int long long
+
+#define sz(x) ((int)x.size())
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define FOR(v, i, l) for(int v = i; v < (int)l; ++v)
+#define REP(v, l) FOR(v, 0, l)
+
+#define MAXN 300005
+#define MAXM 305
+#define MAXB 20
+#define EPS 1e-6
+#define oo (1LL<<60)
+#define MAX 1000000000LL
+#define debug(x) cout << #x << " is " << x << "\n";
+
+const int dx[8] = {1, 1, -1, -1, 2, 2, -2, -2};
+const int dy[8] = {2, -2, 2, -2, 1, -1, 1, -2};
+
+int cases;
+int N, M, K;
+
+main(){
+   double V, N;
+   cin >> V >> N;
+   double total = V*N;
+
+   for(double i = 10.0; i <= 90.0; i+=10.0){
+      cout << (int)ceil((total*i)/100.0) << "\n "[i<90.0];
+   }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
