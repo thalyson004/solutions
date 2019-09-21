db10d8ac811fb9d29cb5ecd566571d35ff3a8051
thalyson004 <thalyson.uece@gmail.com>
2019-09-21 20:38:02 -0300

    Create fliper.cpp

---
+++ OBI_PJ_F1/2014/fliper.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   int P, R;
+   cin >> P >> R;
+   if( P == 0){
+      cout << "C" << endl;
+   }else{
+      if(R==0){
+         cout << "B" << endl;
+      }else{
+         cout << "A" << endl;
+      }
+   }
+}
