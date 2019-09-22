fc12930a001f6ef30dbb68f08f7805706aabdaff
thalyson004 <thalyson.uece@gmail.com>
2019-09-22 14:50:12 -0300

    Create bondinho.cpp

---
+++ OBI_PJ_F1/2017/bondinho.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   int A, M;
+   cin >> A >> M;
+
+   if( A+M <= 50 ){
+      cout << "S" << endl;
+   }else{
+      cout << "N" << endl;
+   }
+}
