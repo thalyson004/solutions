b4a18ad86850f835591b561273abf5e19b6c5a56
thalyson004 <thalyson.uece@gmail.com>
2020-10-04 11:57:40 -0300

    Create B.cpp

---
+++ kickstart/kickstart2020/roundD/B.cpp
@@ -0,0 +1,137 @@
+/*
+   Problem name: Alien Piano
+   Difficulty level: Easy
+   Techniques: Greedy
+   Hints: Read the note
+   Written by Thalyson
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define F first
+#define FF first.first
+#define FS first.second
+#define S second
+#define SF second.first
+#define SS second.second
+#define ii pair<int,int>
+#define iii pair<ii, int>
+#define iiii pair<ii, ii>
+#define viiii vector< iiii >
+#define viii vector< iii >
+#define vii vector< ii >
+#define vi vector< int >
+#define vvi vector<vector< int > >
+#define int int64_t
+
+
+#define sz(x) ((int)x.size())
+#define all(x) x.begin(),x.end()
+#define sortv(V) sort(all(V))
+#define reversev(V) reverse(all(V))
+#define uniquev(V) sortv(V),(V).erase(unique(all(V)),(V).end())
+#define pb push_back
+
+
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define debug(x) cout << #x << " is " << x << "\n";
+
+// Trace dinâmico (parameter pack (c++11))
+template<typename T>
+void println(T a) { cout << a << "\n"; }
+template<typename T, typename... Args>
+void println(T a, Args... args) { cout << a << " "; println(args...); }
+
+template<typename T>
+void print(T a) { cout << a; }
+template<typename T, typename... Args>
+void print(T a, Args... args) { cout << a << " "; print(args...); }
+
+template<typename T>
+void printv(T a, string last="\n") {
+    for(auto el: a)
+      print(el, "");
+    print(last);
+}
+
+template<typename T>
+void scanv(T &a) {
+    for(auto &el: a)
+      cin >> el;
+}
+
+
+//Constants
+#define EPS 1e-6
+#define oo int(1LL<<60)
+#define endl '\n'
+
+//Directions ^>v<
+const int dx4[4] = {-1, 0, 1, 0};
+const int dy4[4] = {0, 1, 0, -1};
+
+const int dx8[8] = {-1, -1, 0, 1, 1,  1,  0, -1};
+const int dy8[8] = { 0,  1, 1, 1, 0, -1, -1, -1};
+
+
+//Main
+int N, M, K, CASES;
+
+main(){
+   std::ios::sync_with_stdio(false);
+   cin.tie(0);
+   cout << setprecision(9) << fixed;
+
+   cin >> CASES;
+   for(int cases = 1; cases <= CASES; cases++){
+      cout << "Case #" << cases << ": ";
+      cin >> N;
+      vi v(N);
+      scanv(v);
+
+      int cur=0;
+      int ans = 0;
+      for(int i = 1; i < N; i++ ){
+         if(v[i]<v[i-1]) cur = min(cur-1, int(-1));
+         else if(v[i]>v[i-1]) cur = max(cur+1, int(1));
+
+         if(abs(cur)==4){
+            ans++;
+            cur=0;
+         }
+      }
+      println(ans);
+   }
+
+   /*
+      Before submit:
+         Check the corners cases
+         Check solution restrictions
+
+      For implementation solutions
+         Check the flow of the variables
+   */
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
