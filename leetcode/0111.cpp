a0218699a71e44097cbaa31412e25fc3eb03285a
thalyson004 <thalyson.uece@gmail.com>
2020-10-22 17:16:57 -0300

    Create 0111.cpp

---
+++ leetcode/0111.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+    	if(root==NULL) return 0;
+	if(root->left ==NULL and root->right==NULL ) return 1;
+	if(root->left != NULL and root->right!=NULL) return min(minDepth(root->left),minDepth(root->right))+1;
+
+	if(root->left!=NULL) return minDepth(root->left)+1;
+	else return minDepth(root->right)+1;
+    }
+};
