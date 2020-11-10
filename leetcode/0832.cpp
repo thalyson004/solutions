d61f9dc617766a0371ac787e7555db3a3e509862
thalyson004 <thalyson.uece@gmail.com>
2020-11-10 07:25:06 -0300

    Update 0832.cpp

--- leetcode/0832.cpp
+++ leetcode/0832.cpp
@@ -4,17 +4,13 @@ public:
         for(vector<int> &row:A){
             int left = 0;
             int right = row.size()-1;
-            while(left<right){
+            while(left<=right){
                 if(row[left]==row[right]){
                     row[left] = row[right] = !row[left];
                 }
                 left++;
                 right--;
             }
-            if(left==right){
-                row[left] ^= 1;
-            }
-            
         }   
         return A;
     }
