8586602b0c8e5e1f894dfd0a571760eed74f3575
thalyson004 <thalyson.uece@gmail.com>
2021-07-31 07:26:20 -0300

    Create 0042_TrappingRainWater.cpp

---
+++ leetcode/0042_TrappingRainWater.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if(!n) return 0;
+        vector<int> left(n), right(n);
+        
+        left[0] = height[0];
+        for(int i = 1; i < n; i++){
+            left[i] = max(left[i-1], height[i]);
+        }
+        
+        right[n-1] = height[n-1];
+        for(int i = n-2; i >= 0; i--){
+            right[i] = max(right[i+1], height[i]);
+        }
+        
+        int ans = 0;
+        for(int i = 1; i < n-1; i++){
+            int h = min(left[i], right[i]);
+            ans += h-height[i];
+        }
+        return ans;
+        
+    }
+};
