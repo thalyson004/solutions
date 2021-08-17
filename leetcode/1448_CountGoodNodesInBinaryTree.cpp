22a120e6078beea937844188a6451aa4c9ed8319
thalyson004 <thalyson.uece@gmail.com>
2021-08-17 08:52:36 -0300

    Create 1448_CountGoodNodesInBinaryTree.cpp

---
+++ leetcode/1448_CountGoodNodesInBinaryTree.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int goodNodes(TreeNode* root, int mx = INT_MIN) {
+        if(root==nullptr) return 0;
+        return (root->val >= mx) 
+                + goodNodes(root->left, max(mx, root->val)) 
+                + goodNodes(root->right, max(mx, root->val));
+    }
+};
