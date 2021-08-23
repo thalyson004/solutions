0f5e282f61d386d7a5fe07acdbb2758b431f99ce
thalyson004 <thalyson.uece@gmail.com>
2021-08-23 11:42:52 -0300

    Create 1018.py

---
+++ uri/1018.py
@@ -0,0 +1,36 @@
+# 1018 Cédulas
+notas001 = int(input())
+
+print(notas001)
+
+#100
+notas100 = notas001//100
+notas001 = notas001%100
+
+#50
+notas050 = notas001//50
+notas001 = notas001%50
+
+#20
+notas020 = notas001//20
+notas001 = notas001%20
+
+#10
+notas010 = notas001//10
+notas001 = notas001%10
+
+#5
+notas005 = notas001//5
+notas001 = notas001%5
+
+#2
+notas002 = notas001//2
+notas001 = notas001%2
+
+print(f"{notas100} nota(s) de R$ 100,00")
+print(f"{notas050} nota(s) de R$ 50,00")
+print(f"{notas020} nota(s) de R$ 20,00")
+print(f"{notas010} nota(s) de R$ 10,00")
+print(f"{notas005} nota(s) de R$ 5,00")
+print(f"{notas002} nota(s) de R$ 2,00")
+print(f"{notas001} nota(s) de R$ 1,00")
