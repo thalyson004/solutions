c173dae313bae5ae8c6facdcc6bcc8d61a57bd07
thalyson004 <thalyson.uece@gmail.com>
2021-10-16 14:27:36 -0300

    Create 1041.py

---
+++ uri/1041.py
@@ -0,0 +1,17 @@
+# URI Online Judge | 1041
+x, y = map(float, input().split())
+
+if x>0 and y>0:
+  print("Q1")
+elif x>0 and y<0:
+  print("Q4")
+elif x<0 and y>0:
+  print("Q2")
+elif x<0 and y<0:
+  print("Q3")
+elif y==0 and x!=0:
+  print("Eixo X")
+elif x==0 and y!=0:
+  print("Eixo Y")
+elif x==0 and y==0:
+  print("Origem")
