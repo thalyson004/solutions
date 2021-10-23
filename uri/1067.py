f580e80859b278caa42321cd595ac3724363809c
thalyson004 <thalyson.uece@gmail.com>
2021-10-23 09:23:09 -0300

    Create 1067.py

---
+++ uri/1067.py
@@ -0,0 +1,17 @@
+# 1067
+
+valor = int(input())
+
+'''
+# valor inicial
+i = 1
+# valor final
+while i<valor+1:
+  # passos
+  print(i)
+  # atualizar 
+  i = i+2
+'''
+
+for i in range(1,valor+1,2):
+  print(i)
