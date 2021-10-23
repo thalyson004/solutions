dad2722500f509e0719c0682f562e8d6dfcbb1aa
thalyson004 <thalyson.uece@gmail.com>
2021-10-23 09:23:23 -0300

    Create 1078.py

---
+++ uri/1078.py
@@ -0,0 +1,12 @@
+# 1078
+valor = int(input())
+
+'''
+for i in range(1, 11): # [0,1,2,3,4,5...9]
+  print(f"{i} x {valor} = {i*valor}")
+'''
+i = 1
+while i<=10:
+  print(f"{i} x {valor} = {i*valor}")
+  i = i+1
+  
