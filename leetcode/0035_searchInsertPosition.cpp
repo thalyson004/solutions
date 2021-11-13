d1ade2142a4bd9f719dde3517c3749776ed2c7c7
thalyson004 <thalyson.uece@gmail.com>
2021-11-13 20:35:59 -0300

    Create 0035_searchInsertPosition.cpp

---
+++ leetcode/0035_searchInsertPosition.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size();
+        
+        while(left < right){
+            int mid = (left+right)/2;
+            
+            if( nums[mid]<target ){
+                left = mid+1;
+            }else{
+                right = mid;         
+            }
+        }
+        
+        return left;
+    }
+};
