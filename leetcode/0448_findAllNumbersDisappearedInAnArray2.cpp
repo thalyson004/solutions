8c590a764003811fcc17d92b18a8a59dc479637c
thalyson004 <thalyson.uece@gmail.com>
2021-11-18 07:09:37 -0300

    Update 0448_findAllNumbersDisappearedInAnArray2.cpp

--- leetcode/0448_findAllNumbersDisappearedInAnArray2.cpp
+++ leetcode/0448_findAllNumbersDisappearedInAnArray2.cpp
@@ -2,14 +2,9 @@ class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         vector<bool> inside(nums.size(), false);
-        for(int num: nums) {
-            if(num>=1 and num <= nums.size()){
-                inside[num-1]=true;
-            }
-        }
-        
+        for(int num: nums) inside[num-1]=true;
+ 
         vector<int> ans;
-        
         for(int i = 0; i < nums.size(); i++){
             if(!inside[i]){
                 ans.push_back(i+1);
